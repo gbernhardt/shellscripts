e972b2048b558d9fc2f554c42525dfadf75cbcf4
Gerd Bernhardt <info@metaoffice.de>
2013-11-24 16:03:38 +0100

    just try something

--- handbrake.sh
+++ handbrake.sh
@@ -10,5 +10,4 @@ SUBTITLES="--subtitle scan"
 echo "HandBrakeCLI $SRC $DEST -m $AUDIO $VIDEO $SUBTITLES"
 HandBrakeCLI $SRC $DEST -m $AUDIO $VIDEO $SUBTITLES
 
-
 #level=4.1:ref=4:b-adapt=2:direct=auto:me=umh:subq=8:rc-lookahead=50:psy-rd=1.0,0.15:deblock=-1,-1:vbv-bufsize=30000:vbv-maxrate=40000:slices=4\'\'ª
